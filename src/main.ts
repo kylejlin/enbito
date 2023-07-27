f59b9e162cfb8a341cf4585e13527241acad3011
Kyle Lin <kylejlin.us@gmail.com>
2023-07-27 01:28:11 -0700

    Start prototyping procgen

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,9 @@ import {
   PlaneGeometry,
   AnimationMixer,
   AnimationClip,
+  InstancedMesh,
+  Matrix4,
+  MeshStandardMaterial,
 } from "three";
 import { Sky } from "three/addons/objects/Sky.js";
 import { RepeatWrapping } from "three";
@@ -102,6 +105,10 @@ export function main(assets: Assets): void {
   const playerMixer = new AnimationMixer(player);
   const playerWalkAction = playerMixer.clipAction(playerWalkClip);
   playerWalkAction.play();
+
+  const world = getUninitializedWorld(123);
+  renderChunkIfMissing(world, 0, 0);
+  addWorldToScene(world, scene);
 
   addSky();
   addEnvironment();
@@ -190,14 +197,196 @@ export function main(assets: Assets): void {
   function addEnvironment(): void {
     scene.environment = assets.environment;
   }
+}
 
-  Perlin.seed(123);
-  let min = Infinity;
-  let max = -Infinity;
-  for (let i = 0; i < 1000; ++i) {
-    const y = Perlin.simplex2(i, 0);
-    min = Math.min(min, y);
-    max = Math.max(max, y);
+interface World {
+  seed: number;
+  chunks: { [key: string]: Chunk };
+  blockMeshes: BlockMeshDict;
+}
+
+type BlockMeshDict = { [key in BlockType]: Mesh };
+
+interface Chunk {
+  blocks: Uint16Array;
+}
+
+function getUninitializedWorld(seed: number): World {
+  return { seed, chunks: {}, blockMeshes: getBlockMeshes() };
+}
+
+const BLOCK_SIZE = 2;
+
+function getBlockMeshes(): BlockMeshDict {
+  return {
+    // Don't care. We never render the `Blank` mesh.
+    [BlockTypes.Blank]: new Mesh(
+      new BoxGeometry(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE),
+      new MeshStandardMaterial({ color: 0xff0000 })
+    ),
+
+    [BlockTypes.Dirt]: new Mesh(
+      new BoxGeometry(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE),
+      new MeshStandardMaterial({ color: 0x00ff00 })
+    ),
+  };
+}
+
+function renderChunkIfMissing(world: World, x: number, z: number): void {
+  const k = getCoordString(x, z);
+  if (world.chunks[k]) {
+    return;
   }
-  console.log({ min, max });
+
+  world.chunks[k] = generateChunk(world.seed, x, z);
+}
+
+const CHUNK_WIDTH = 16;
+const CHUNK_HEIGHT = 256;
+
+const BlockTypes = {
+  Blank: 0,
+  Dirt: 1,
+} as const;
+type BlockType = (typeof BlockTypes)[keyof typeof BlockTypes];
+
+function generateChunk(seed: number, chunkX: number, chunkZ: number): Chunk {
+  const blocks = new Uint16Array(CHUNK_WIDTH * CHUNK_WIDTH * CHUNK_HEIGHT);
+  blocks.fill(BlockTypes.Blank);
+
+  // TODO: Implement real chunk generation.
+  // This is just a placeholder.
+  for (let i = 0; i < blocks.length; ++i) {
+    const [localX, localY, localZ] = getBlockCoordsFromBlockIndex(i);
+    if (localY > 10) {
+      blocks[i] = BlockTypes.Dirt;
+    }
+  }
+
+  return { blocks };
+}
+
+function getCoordString(x: number, z: number): string {
+  return `${x},${z}`;
+}
+
+function addWorldToScene(world: World, scene: Scene): void {
+  for (const k in world.chunks) {
+    const [x, z] = parseCoordString(k);
+    addChunkToScene(world.chunks[k], x, z, scene, world.blockMeshes);
+  }
+}
+
+function parseCoordString(coordString: string): [number, number] {
+  const [x, z] = coordString.split(",").map((s) => Number.parseInt(s, 10));
+
+  if (Number.isNaN(x) || Number.isNaN(z)) {
+    throw new Error(`Invalid coord string: ${coordString}`);
+  }
+
+  return [x, z];
+}
+
+function addChunkToScene(
+  chunk: Chunk,
+  chunkX: number,
+  chunkZ: number,
+  scene: Scene,
+  dict: BlockMeshDict
+): void {
+  const factoryDict = getEmptyBlockInstancedMeshFactoryDict();
+  for (let i = 0; i < chunk.blocks.length; ++i) {
+    const [localX, localY, localZ] = getBlockCoordsFromBlockIndex(i);
+    const blockType = chunk.blocks[i] as BlockType;
+    const factory = factoryDict[blockType];
+    factory.localCoords.push(localX, localY, localZ);
+  }
+  addPendingBlocksToScene(factoryDict, chunkX, chunkZ, scene, dict);
+}
+
+type BlockInstancedMeshFactoryDict = {
+  [key in BlockType]: BlockInstancedMeshFactory;
+};
+
+interface BlockInstancedMeshFactory {
+  localCoords: number[];
+}
+
+function getEmptyBlockInstancedMeshFactoryDict(): BlockInstancedMeshFactoryDict {
+  return {
+    [BlockTypes.Blank]: { localCoords: [] },
+    [BlockTypes.Dirt]: { localCoords: [] },
+  };
+}
+
+/**
+ * The input must be a non-negative integer.
+ * The output will be a tuple of non-negative integers.
+ */
+function getBlockCoordsFromBlockIndex(index: number): [number, number, number] {
+  const x = index % CHUNK_WIDTH;
+  const z = Math.floor((index % (CHUNK_WIDTH * CHUNK_WIDTH)) / CHUNK_WIDTH);
+  const y = Math.floor(index / (CHUNK_WIDTH * CHUNK_WIDTH));
+
+  return [x, y, z];
+}
+
+/**
+ * All inputs must be non-negative integers.
+ * The output will be a non-negative integer.
+ */
+function getBlockIndexFromBlockCoords(x: number, y: number, z: number): number {
+  return x + z * CHUNK_WIDTH + y * CHUNK_WIDTH * CHUNK_WIDTH;
+}
+
+function addPendingBlocksToScene(
+  factoryDict: BlockInstancedMeshFactoryDict,
+  x: number,
+  z: number,
+  scene: Scene,
+  meshDict: BlockMeshDict
+): void {
+  for (const blockType of Object.values(BlockTypes)) {
+    if (blockType === BlockTypes.Blank) {
+      continue;
+    }
+
+    const factory = factoryDict[blockType];
+    const mesh = meshDict[blockType];
+    const instanced = getInstancedMeshFromFactory(factory, x, z, mesh);
+    scene.add(instanced);
+  }
+}
+
+function getInstancedMeshFromFactory(
+  factory: BlockInstancedMeshFactory,
+  chunkX: number,
+  chunkZ: number,
+  mesh: Mesh
+): InstancedMesh {
+  const chunkXScaled = chunkX * CHUNK_WIDTH * BLOCK_SIZE;
+  const chunkZScaled = chunkZ * CHUNK_WIDTH * BLOCK_SIZE;
+
+  const len = getBlockLength(factory);
+  const instanced = new InstancedMesh(mesh.geometry, mesh.material, len);
+
+  for (let i = 0; i < len; ++i) {
+    const localX = factory.localCoords[i * 3 + 0];
+    const localY = factory.localCoords[i * 3 + 1];
+    const localZ = factory.localCoords[i * 3 + 2];
+    instanced.setMatrixAt(
+      i,
+      new Matrix4().makeTranslation(
+        chunkXScaled + localX * BLOCK_SIZE,
+        localY * BLOCK_SIZE,
+        chunkZScaled + localZ * BLOCK_SIZE
+      )
+    );
+  }
+
+  return instanced;
+}
+
+function getBlockLength(factory: BlockInstancedMeshFactory): number {
+  return factory.localCoords.length / 3;
 }
