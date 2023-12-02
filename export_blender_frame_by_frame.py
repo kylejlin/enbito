c990b57b03371fe7671ecbaf07452e293aaad9fc
Kyle Lin <kylejlin.us@gmail.com>
2023-12-02 15:18:53 +0900

    Add export script

---
+++ export_blender_frame_by_frame.py
@@ -0,0 +1,18 @@
+import bpy
+
+# First frame to export
+frame_start = 1
+
+# Last frame to export
+frame_end = 29
+
+# Filename
+filename = "/Users/kyle/enbito/azuki_spear_static"
+
+for f in range(frame_start, frame_end + 1):
+    bpy.context.scene.frame_set(f)
+    bpy.ops.export_scene.gltf(
+        filepath="%s.f%04d.glb" % (filename, f),
+        export_animations=False,
+        export_current_frame=True,
+    )
