b67ea19af4f491f7213970bfa24f680da576c724
Kyle Lin <kylejlin.us@gmail.com>
2023-12-07 21:31:32 +0900

    Update Assets["azukiUnarmedExplosionFrames"]

--- src/assets.ts
+++ src/assets.ts
@@ -5,12 +5,9 @@ import {
   LoadingManager,
   Texture,
   TextureLoader,
-  Group,
 } from "three";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
-import { MTLLoader } from "three/addons/loaders/MTLLoader.js";
 import { Tuple24, Tuple29 } from "./nTuple";
 
 export const BLENDER_FPS = 24;
@@ -40,9 +37,8 @@ export interface Assets {
   azukiSpearStabFrames: Tuple24<GLTF>;
   edamameSpearWalkFrames: Tuple29<GLTF>;
   edamameSpearStabFrames: Tuple24<GLTF>;
-  // TODO Rename to `azukiExplodingFrames` to be more consistent.
-  explodingAzukiFrames: Tuple29<Group>;
-  explodingEdamameFrames: Tuple29<Group>;
+  azukiUnarmedExplosionFrames: Tuple29<GLTF>;
+  edamameUnarmedExplosionFrames: Tuple29<GLTF>;
 }
 
 export interface ModelConstants {
@@ -221,66 +217,66 @@ export function loadAssets(): Promise<Assets> {
       getEdamameSpearStabFrame("f0024"),
     ]),
     Promise.all([
-      getAzukiExplodingFrame("000001"),
-      getAzukiExplodingFrame("000002"),
-      getAzukiExplodingFrame("000003"),
-      getAzukiExplodingFrame("000004"),
-      getAzukiExplodingFrame("000005"),
-      getAzukiExplodingFrame("000006"),
-      getAzukiExplodingFrame("000007"),
-      getAzukiExplodingFrame("000008"),
-      getAzukiExplodingFrame("000009"),
-      getAzukiExplodingFrame("000010"),
-      getAzukiExplodingFrame("000011"),
-      getAzukiExplodingFrame("000012"),
-      getAzukiExplodingFrame("000013"),
-      getAzukiExplodingFrame("000014"),
-      getAzukiExplodingFrame("000015"),
-      getAzukiExplodingFrame("000016"),
-      getAzukiExplodingFrame("000017"),
-      getAzukiExplodingFrame("000018"),
-      getAzukiExplodingFrame("000019"),
-      getAzukiExplodingFrame("000020"),
-      getAzukiExplodingFrame("000021"),
-      getAzukiExplodingFrame("000022"),
-      getAzukiExplodingFrame("000023"),
-      getAzukiExplodingFrame("000024"),
-      getAzukiExplodingFrame("000025"),
-      getAzukiExplodingFrame("000026"),
-      getAzukiExplodingFrame("000027"),
-      getAzukiExplodingFrame("000028"),
-      getAzukiExplodingFrame("000029"),
+      getAzukiExplodingFrame("f0001"),
+      getAzukiExplodingFrame("f0002"),
+      getAzukiExplodingFrame("f0003"),
+      getAzukiExplodingFrame("f0004"),
+      getAzukiExplodingFrame("f0005"),
+      getAzukiExplodingFrame("f0006"),
+      getAzukiExplodingFrame("f0007"),
+      getAzukiExplodingFrame("f0008"),
+      getAzukiExplodingFrame("f0009"),
+      getAzukiExplodingFrame("f0010"),
+      getAzukiExplodingFrame("f0011"),
+      getAzukiExplodingFrame("f0012"),
+      getAzukiExplodingFrame("f0013"),
+      getAzukiExplodingFrame("f0014"),
+      getAzukiExplodingFrame("f0015"),
+      getAzukiExplodingFrame("f0016"),
+      getAzukiExplodingFrame("f0017"),
+      getAzukiExplodingFrame("f0018"),
+      getAzukiExplodingFrame("f0019"),
+      getAzukiExplodingFrame("f0020"),
+      getAzukiExplodingFrame("f0021"),
+      getAzukiExplodingFrame("f0022"),
+      getAzukiExplodingFrame("f0023"),
+      getAzukiExplodingFrame("f0024"),
+      getAzukiExplodingFrame("f0025"),
+      getAzukiExplodingFrame("f0026"),
+      getAzukiExplodingFrame("f0027"),
+      getAzukiExplodingFrame("f0028"),
+      getAzukiExplodingFrame("f0029"),
     ]),
     Promise.all([
-      getEdamameExplodingFrame("000001"),
-      getEdamameExplodingFrame("000002"),
-      getEdamameExplodingFrame("000003"),
-      getEdamameExplodingFrame("000004"),
-      getEdamameExplodingFrame("000005"),
-      getEdamameExplodingFrame("000006"),
-      getEdamameExplodingFrame("000007"),
-      getEdamameExplodingFrame("000008"),
-      getEdamameExplodingFrame("000009"),
-      getEdamameExplodingFrame("000010"),
-      getEdamameExplodingFrame("000011"),
-      getEdamameExplodingFrame("000012"),
-      getEdamameExplodingFrame("000013"),
-      getEdamameExplodingFrame("000014"),
-      getEdamameExplodingFrame("000015"),
-      getEdamameExplodingFrame("000016"),
-      getEdamameExplodingFrame("000017"),
-      getEdamameExplodingFrame("000018"),
-      getEdamameExplodingFrame("000019"),
-      getEdamameExplodingFrame("000020"),
-      getEdamameExplodingFrame("000021"),
-      getEdamameExplodingFrame("000022"),
-      getEdamameExplodingFrame("000023"),
-      getEdamameExplodingFrame("000024"),
-      getEdamameExplodingFrame("000025"),
-      getEdamameExplodingFrame("000026"),
-      getEdamameExplodingFrame("000027"),
-      getEdamameExplodingFrame("000028"),
-      getEdamameExplodingFrame("000029"),
+      getEdamameExplodingFrame("f0001"),
+      getEdamameExplodingFrame("f0002"),
+      getEdamameExplodingFrame("f0003"),
+      getEdamameExplodingFrame("f0004"),
+      getEdamameExplodingFrame("f0005"),
+      getEdamameExplodingFrame("f0006"),
+      getEdamameExplodingFrame("f0007"),
+      getEdamameExplodingFrame("f0008"),
+      getEdamameExplodingFrame("f0009"),
+      getEdamameExplodingFrame("f0010"),
+      getEdamameExplodingFrame("f0011"),
+      getEdamameExplodingFrame("f0012"),
+      getEdamameExplodingFrame("f0013"),
+      getEdamameExplodingFrame("f0014"),
+      getEdamameExplodingFrame("f0015"),
+      getEdamameExplodingFrame("f0016"),
+      getEdamameExplodingFrame("f0017"),
+      getEdamameExplodingFrame("f0018"),
+      getEdamameExplodingFrame("f0019"),
+      getEdamameExplodingFrame("f0020"),
+      getEdamameExplodingFrame("f0021"),
+      getEdamameExplodingFrame("f0022"),
+      getEdamameExplodingFrame("f0023"),
+      getEdamameExplodingFrame("f0024"),
+      getEdamameExplodingFrame("f0025"),
+      getEdamameExplodingFrame("f0026"),
+      getEdamameExplodingFrame("f0027"),
+      getEdamameExplodingFrame("f0028"),
+      getEdamameExplodingFrame("f0029"),
     ]),
     new Promise<GLTF>((resolve) => {
       new GLTFLoader().load("./models/azuki_spear_baked.glb", (gltf) => {
@@ -302,8 +298,8 @@ export function loadAssets(): Promise<Assets> {
       azukiSpearStabFrames,
       edamameSpearWalkFrames,
       edamameSpearStabFrames,
-      explodingAzukiFrames,
-      explodingEdamameFrames,
+      azukiUnarmedExplosionFrames,
+      edamameUnarmedExplosionFrames,
       azukiSpearBaked,
     ]): Assets => {
       console.log({ azukiSpearBaked });
@@ -354,8 +350,8 @@ export function loadAssets(): Promise<Assets> {
         azukiSpearStabFrames,
         edamameSpearWalkFrames,
         edamameSpearStabFrames,
-        explodingAzukiFrames,
-        explodingEdamameFrames,
+        azukiUnarmedExplosionFrames,
+        edamameUnarmedExplosionFrames,
       };
     }
   );
@@ -405,33 +401,23 @@ function getEdamameSpearStabFrame(id: string): Promise<GLTF> {
   });
 }
 
-function getAzukiExplodingFrame(id: string): Promise<Group> {
-  return new Promise<Group>((resolve) => {
-    new MTLLoader().load(
-      `./models/soldier_unarmed_exploding_frames/azuki_unarmed_common.mtl`,
-      (mtl) => {
-        new OBJLoader()
-          .setMaterials(mtl)
-          .load(
-            `./models/soldier_unarmed_exploding_frames/azuki_unarmed_exploding_${id}.obj`,
-            resolve
-          );
+function getAzukiExplodingFrame(id: string): Promise<GLTF> {
+  return new Promise<GLTF>((resolve) => {
+    new GLTFLoader().load(
+      `./models/azuki_unarmed_frames/explode/explode.${id}.glb`,
+      (gltf) => {
+        resolve(gltf);
       }
     );
   });
 }
 
-function getEdamameExplodingFrame(id: string): Promise<Group> {
-  return new Promise<Group>((resolve) => {
-    new MTLLoader().load(
-      `./models/soldier_unarmed_exploding_frames/edamame_unarmed_common.mtl`,
-      (mtl) => {
-        new OBJLoader().setMaterials(mtl).load(
-          // This is not a typo.
-          // We use the same mesh for both azuki and edamame soldier explosions.
-          `./models/soldier_unarmed_exploding_frames/azuki_unarmed_exploding_${id}.obj`,
-          resolve
-        );
+function getEdamameExplodingFrame(id: string): Promise<GLTF> {
+  return new Promise<GLTF>((resolve) => {
+    new GLTFLoader().load(
+      `./models/edamame_unarmed_frames/explode/explode.${id}.glb`,
+      (gltf) => {
+        resolve(gltf);
       }
     );
   });
