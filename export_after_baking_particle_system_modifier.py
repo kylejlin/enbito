15a76b2a2de2154a89d318d212b9313ee471ef20
Kyle Lin <kylejlin.us@gmail.com>
2023-12-07 21:53:12 +0900

    Remove armature modifier in azuki explosion

---
+++ export_after_baking_particle_system_modifier.py
@@ -0,0 +1,315 @@
+# IMPORTANT
+#
+# 1. Make sure to select the correct animation.
+# 2. Make sure to select the body mesh
+#    (i.e., the mesh with the armature modifier).
+# 3. This script may mess up the .blend file.
+#    So, after you run this script,
+#    do **not** save the .blend file.
+
+import bpy
+
+# Filename
+filename = "/Users/kyle/enbito/public/models/azuki_unarmed_frames/explode/explode"
+
+modifier_name = "ParticleSystem"
+
+# For some reason, the script doesn't work if I
+# use a for-loop.
+# So, I have to "unwrap" the loop
+# (i.e., manually copy and paste the code).
+
+f = 1
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 2
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 3
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 4
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 5
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 6
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 7
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 8
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 9
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 10
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 11
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 12
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 13
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 14
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 15
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 16
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 17
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 18
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 19
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 20
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 21
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 22
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 23
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 24
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+
+# Comment out the below code if you are exporting
+# the Slow Stab (which doesn't change from frame 25-29).
+
+f = 25
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 26
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 27
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 28
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
+f = 29
+bpy.context.scene.frame_set(f)
+bpy.ops.object.modifier_apply(modifier=modifier_name)
+bpy.ops.export_scene.gltf(
+    filepath="%s.f%04d.glb" % (filename, f),
+    export_animations=False,
+    export_skins=False,
+    export_current_frame=True,
+)
+bpy.ops.ed.undo()
