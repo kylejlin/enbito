8baeae6b85b58c31d147d543b259f8c585e4cf71
Kyle Lin <kylejlin.us@gmail.com>
2023-12-15 12:18:30 +0900

    Extract tick.ts

---
+++ src/gameConsts.ts
@@ -0,0 +1,17 @@
+export const MILLISECS_PER_TICK = 10;
+
+export const TURN_SPEED_RAD_PER_SEC = Math.PI * 0.5;
+export const SPEAR_ATTACK_RANGE_SQUARED = 8 ** 2;
+export const STAB_DAMAGE = 60;
+export const STAB_COOLDOWN = 1;
+export const SLASH_DAMAGE = 40;
+export const SOLDIER_DEPLOYMENT_DELAY_SECONDS = 1;
+export const ASSEMBLING_TROOP_SPEEDUP_FACTOR = 2;
+export const MAX_LANDING_SPEED = 30;
+export const DRAGONFLY_MOUNTING_MAX_DISTANCE_SQUARED = 5 ** 2;
+export const DRAGONFLY_MIN_SPEED = 5;
+export const STAB_TIME_SCALE = 2;
+export const MAX_STAB_DELAY = 1;
+export const DRAGONFLY_MAX_SPEED = 50;
+export const DRAGONFLY_ACCELERATION = 10;
+export const DRAGONFLY_DECELERATION = 30;
---
+++ src/resourceBundle.ts
@@ -0,0 +1,33 @@
+import { Assets } from "./assets";
+import { BattleState } from "./battleState";
+import { San } from "./san";
+
+export interface Resources {
+  assets: Assets;
+
+  mouse: MouseState;
+  keys: KeySet;
+
+  battle: BattleState;
+
+  san: San;
+}
+
+export interface MouseState {
+  /** Left edge is 0, right edge is 1. */
+  x: number;
+  /** Top edge is 0, bottom edge is 1. */
+  y: number;
+  isLocked: boolean;
+}
+
+export interface KeySet {
+  w: boolean;
+  f: boolean;
+  t: boolean;
+  g: boolean;
+  r: boolean;
+  v: boolean;
+  space: boolean;
+  _1: boolean;
+}
